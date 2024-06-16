1f936d69bb37436f211e2e090812e8b94685599f
emiry <emiry@192.168.1.72>
2024-06-16 12:17:33 -0600

    New Project Italy

---
+++ src/dao/tables.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author emiry
+ */
+public class tables {
+    public static void main(String[] args) {
+        try {
+            String userTable = "create table user(id int AUTO_INCREMENT primary key,name varchar(200), email varchar(200),mobileNumber varchar(200),password varchar(200), status varchar(20),UNIQUE (email))";
+            String adminDetails = "insert into user(name,email,mobileNumber,password,status) values('Admin','admin@gmail.com','1234567890','cbtis2008','true')";
+            String categoryTable = "create table category(id int AUTO_INCREMENT primary key,name varchar(200))";
+            String productTable = "create table product(id int AUTO_INCREMENT primary key,name varchar(200),category varchar(200), price varchar(200))";
+            DbOperations.setDataorDelete(productTable, "Tabla de Productos creada");
+            DbOperations.setDataorDelete(userTable, "Tabla de Usuario Creada");
+            DbOperations.setDataorDelete(adminDetails, "Usuario de Admin Creado");
+            DbOperations.setDataorDelete(categoryTable, "Tabla de Category Creada");
+        }
+        catch(Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+                }
+    }
+}
